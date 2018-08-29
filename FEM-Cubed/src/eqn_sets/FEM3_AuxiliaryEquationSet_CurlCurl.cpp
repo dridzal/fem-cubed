fb1463836ee50d52edfdce7e38f72608bf15bece
Denis Ridzal <dridzal@sandia.gov>
2018-08-29 13:41:50 -0600

    Minor update.

--- FEM-Cubed/src/eqn_sets/FEM3_AuxiliaryEquationSet_CurlCurl.cpp
+++ FEM-Cubed/src/eqn_sets/FEM3_AuxiliaryEquationSet_CurlCurl.cpp
@@ -3,4 +3,4 @@
 #include "FEM3_AuxiliaryEquationSet_CurlCurl.hpp"
 #include "FEM3_AuxiliaryEquationSet_CurlCurl_impl.hpp"
 
-PANZER_INSTANTIATE_TEMPLATE_CLASS_ONE_T(mini_em::AuxiliaryEquationSet_CurlCurl)
+PANZER_INSTANTIATE_TEMPLATE_CLASS_ONE_T(FEM3::AuxiliaryEquationSet_CurlCurl)
